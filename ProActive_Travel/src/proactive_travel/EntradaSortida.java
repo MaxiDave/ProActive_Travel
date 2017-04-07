bc0f642a41478a928c35f28b9f7022599a0502af
TheMasterBoss <roger.barnes11@gmail.com>
2017-04-07 17:30:01 +0200

    S'ha afegit EntradaSortidfa, CalculRutaMapa i Ruta. Mig afegit MitjaTransport

---
+++ ProActive_Travel/src/proactive_travel/EntradaSortida.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proactive_travel;
+
+import java.util.*;
+
+/**
+ *
+ * @author Roger
+ */
+public class EntradaSortida {
+    
+    /**
+     * @pre: --
+     * @post: Demana la ruta del fitxer a carregar dades i el retorna en lectura preparat per llegir
+     * @return 
+     */
+    public Fitxer entrarNomFitxer(){
+        
+    }
+    
+    /**
+     * @pre: --
+     * @post: Demana les dades per a poder crear un GrupClients a partir dels Clients de l’agència
+     * @param clients
+     * @return 
+     */
+    public grupClients crearGrup(Collection<Client> clients){
+        
+    }
+    
+    /**
+     * @pre: --
+     * @post: Mostra la Ruta “resultat” per la sortida estàndard 
+     * @param resultat 
+     */
+    public void mostrarRuta(Ruta resultat){
+        
+    }
+}
---
+++ ProActive_Travel/src/proactive_travel/Ruta.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proactive_travel;
+
+import java.util.*;
+
+/**
+ *
+ * @author Roger
+ */
+public class Ruta {
+    
+    /**
+     * @pre: --
+     * @post: Crea una Ruta inicial buida
+     */
+    public Ruta(){
+        
+    }
+    
+    /**
+     * @pre: --
+     * @post: Afegeix un subtrajecte a la Ruta
+     * @param traj 
+     */
+    public void afegeixSubtrajecte(Trajecte traj){
+        
+    }
+    
+    /**
+     * @pre: --
+     * @post: Retorna cert si el Trajecte absolut de la ruta té “origen” com origen i “desti” com destí, i també opcionalment passa per tots els “punts”. És a dir, si la Ruta està completada.
+     * @param o
+     * @param d
+     * @param punts
+     * @return 
+     */
+    public Boolean esCompleta(PuntInteres o, PuntInteres d, Collection<PuntInteres> punts){
+        
+    }
+    
+    /**
+     * @pre: --
+     * @post: Retorna cert si la Ruta es buida, fals altrament
+     * @return 
+     */
+    public Boolean isEmpty(){
+        
+    }
+}
