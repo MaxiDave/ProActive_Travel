e80c9612d513a1e977d08c15609961b089787476
MaxiDave <maxidave13@gmail.com>
2017-04-07 08:45:52 +0200

    Modificat diagrama i Classe clients

--- ProActive_Travel/src/proactive_travel/Client.java
+++ ProActive_Travel/src/proactive_travel/Client.java
@@ -8,14 +8,25 @@ package proactive_travel;
 import java.util.*;
 
 public class Client {
+    
+    private String nom;
+    private Vector<String> preferencies;
+    
     /** @pre: --
         @post: Es crea un client amb nom “n” i preferències “prefs” */
-    Client(String n, Vector<Preferencia> prefs){
-        
+    public Client(String n, Vector<String> prefs){
+        nom= n;
+        preferencies= prefs;
+    }
+    
+    /** @pre: --
+        @post: Retorna el nom del client */
+    public String obtenirNom(){
+        return nom;
     }
     /** @pre: --
         @post: Retorna un iterador a les preferències del client */
-    Iterator<Preferencia> obtenirPreferencies(){
-        
+    public Iterator<String> obtenirPreferencies(){
+        return preferencies.iterator();
     }
 }
