779bd05bf094caecd5904c9af9c6412c76c9b940
TheMasterBoss <roger.barnes11@gmail.com>
2017-05-24 16:26:55 +0200

    Nou: -> Ara ja no es Viatge1.txt s'ha canviat a Resultat.txt Falta: -> La ruta per anar a l'hotel i per tornar -> Zones horaries -> Lo de categoria hotels -> I finalment sortidaKML amb MTIndirectes -> Documentar Mapa els meus

--- ProActive_Travel/src/proactive_travel/CalculGreedy.java
+++ ProActive_Travel/src/proactive_travel/CalculGreedy.java
@@ -118,7 +118,7 @@ public abstract class CalculGreedy {
         else{
             ll.add(null);
         }
-        Sortida.mostrarRutes(ll, clients, "viatge1.txt");
+        Sortida.mostrarRutes(ll, clients, "Resultat.txt");
         return ll;
     }
     
